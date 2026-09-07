1528535de34f42aaba3c830196c3036613140a1c
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-09-07 15:13:49 +0300

    Choice, Question models added

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -98,6 +98,28 @@ class Enrollment(models.Model):
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    grade = models.IntegerField(default=50)
+
+    def __str__(self):
+        return "Question:" + self.content
+    
+    # def is_get_score(self, selected_ids):
+    #     all_answers = self.choice_set.
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=50)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "Choice:" + self.content
+    
+
+class Submission(models.Model):
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   choices = models.ManyToManyField(Choice)
+
